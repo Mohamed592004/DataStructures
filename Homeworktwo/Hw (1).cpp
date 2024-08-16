c7d45d4314e6b3b21c960f48ca1ea76921873e4e
Mohamed592004 <sci_223218@omu.edu.ly>
2024-08-16 14:56:21 +0200

    Add files via upload

---
+++ Homeworktwo/Hw (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+    string name = "Mohamed Ibrahim ";
+    string ID = "223218";
+    
+    cout<<"Name : "<<name<<endl;
+    cout<<"ID : "<< ID<<endl;
+
+    return 0;
+}
